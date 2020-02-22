867d6b8a606f504b4a6f4c9678b6882ffab87dd4
xinrui Bai <bxrbaixinrui@gmail.com>
2020-02-21 20:34:11 -0500

    initial commit

---
+++ Movie/Movie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Bai xinrui on 2/21/20.
+//  Copyright © 2020 Bai xinrui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

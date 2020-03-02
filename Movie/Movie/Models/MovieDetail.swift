de89f84ee6372b73378d035019cc5ca903e3cd50
xinrui Bai <bxrbaixinrui@gmail.com>
2020-03-01 21:16:36 -0500

    introduce codable protocol for Models

--- Movie/Movie/Models/MovieDetail.swift
+++ Movie/Movie/Models/MovieDetail.swift
@@ -8,9 +8,60 @@
 
 import Foundation
 
-struct MovieDetail {
-    var imageURL: String
-    var details: [MovieInfoPair]
+struct MovieDetail: Codable {
+    var imageURL = "https://image.tmdb.org/t/p/w500/rb5HHy0J4yZYlBA1tqL1akWo6WQ.jpg"
+    var language: String?
+    var title: String?
+    var date: String?
+    var overView: String?
+    var details = [MovieInfoPair]()
+    
+    enum CodingKeys: String, CodingKey {
+        case imageURL = "poster_path"
+        case language = "original_language"
+        case title = "original_title"
+        case date = "release_date"
+        case overView = "overview"
+    }
+    
+    init() {
+        
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let imagePath = try values.decode(String?.self, forKey: .imageURL)
+        language = try values.decode(String?.self, forKey: .language)
+        title = try values.decode(String?.self, forKey: .title)
+        date = try values.decode(String?.self, forKey: .date)
+        overView = try values.decode(String?.self, forKey: .overView)
+        
+        if let path = imagePath {
+            imageURL = "\(Constants.imagePrefix)\(path)"
+        }
+        
+        if let language = language {
+            details.append(MovieInfoPair(infoType: "Language: ", content: language))
+        }
+        if let title = title {
+            details.append(MovieInfoPair(infoType: "Title: ", content: title))
+        }
+        if let date = date {
+            details.append(MovieInfoPair(infoType: "Date: ", content: date))
+        }
+        if let overView = overView {
+            details.append(MovieInfoPair(infoType: "Overview: ", content: overView))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(imageURL, forKey: .imageURL)
+        try container.encode(language, forKey: .language)
+        try container.encode(title, forKey: .title)
+        try container.encode(date, forKey: .date)
+        try container.encode(overView, forKey: .overView)
+    }
 }
 
 struct MovieInfoPair {
